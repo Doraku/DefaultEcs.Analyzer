69f78f214f51fe2a33c7d9df0e847a37bd7ab968
Laszlo Paillat <laszlo.paillat@gmail.com>
2020-02-24 13:39:24 +0100

    fixed release.bat path

--- Release.bat
+++ Release.bat
@@ -8,9 +8,9 @@ dotnet test source\DefaultEcs.Analyzer.Test\DefaultEcs.Analyzer.Test.csproj -c R
 
 IF %ERRORLEVEL% GTR 0 GOTO :end
 
-dotnet clean source\DefaultEcs\DefaultEcs.Analyzer.csproj -c Release
-dotnet clean source\DefaultEcs\DefaultEcs.Analyzer.Release.csproj -c Release
+dotnet clean source\DefaultEcs.Analyzer\DefaultEcs.Analyzer.csproj -c Release
+dotnet clean source\DefaultEcs.Analyzer\DefaultEcs.Analyzer.Release.csproj -c Release
 
-dotnet pack source\DefaultEcs\DefaultEcs.Analyzer.Release.csproj -c Release -o package
+dotnet pack source\DefaultEcs.Analyzer\DefaultEcs.Analyzer.Release.csproj -c Release -o package
 
 :end
